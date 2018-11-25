6b293d5d740069230bf4d91cffcec376190e0599
Ageeleshwar Kandavelu <akilesh1597@gmail.com>
2018-11-25 21:31:29 +0530

    test module and setuptools setup.py

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from fluidflow.tests.containers_tests import future_test
+from fluidflow.tests.executables_tests import task_test
+
+
+future_test.run_test()
+task_test.run_test()
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 25 16:37:03 2018
+
+@author: akilesh
+"""
+
+from setuptools import setup, find_packages
+setup(
+    name="fluidflow",
+    version="0.1",
+    packages=find_packages(),
+)
